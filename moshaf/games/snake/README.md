93dd64f2ad0b2a659293a5fd22f513569b5ab7b0
AHMED HAFDI <58536023+HAFDIAHMED@users.noreply.github.com>
2024-01-05 13:17:50 +0100

    remove moshaf
    remove moshaf

--- moshaf/games/snake/README.md
+++
@@ -1,2 +0,0 @@
-# Snake game
-A simple Snake game built with HTML, CSS and JavaScript
--- moshaf/games/tetris/src/onload.js
+++
@@ -1,6 +0,0 @@
-+function(document) {
-  var link = document.createElement("link")
-  link.rel = "stylesheet"
-  link.href = "./src/styles.css" 
-  document.body.appendChild( link )
-}(document)
